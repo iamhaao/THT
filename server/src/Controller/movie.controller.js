d76ad72f98d38e5817ed727ce6c9be8ac9ec6427
letrinh1504 <nguyentrinh154@gmail.com>
2024-03-05 22:25:10 +0700

    UpdateMovie

--- server/src/Controller/movie.controller.js
+++ server/src/Controller/movie.controller.js
@@ -58,4 +58,45 @@ const createMovie = async (req, res, next) => {
       res.status(400).json({ message: error.message });
     }
   };
-  export {createMovie, getMovieById}
+  const updateMovie = async (req, res, next) => {
+    try {
+      const {
+        name,
+        des,
+        image,
+        titleImage,
+        rate,
+        numberOfReviews,
+        category,
+        time,
+        language,
+        year,
+        casts,
+        video,
+      } = req.body;
+      const movieId = req.params.id;
+      const movie = await Movie.findById(req.params.id);
+      if (movie) {
+        movie.name = name || movie.name;
+        movie.des = des || movie.des;
+        movie.image = image || movie.image;
+        movie.titleImage = titleImage || movie.titleImage;
+        movie.rate = rate || movie.rate;
+        movie.category = category || movie.category;
+        movie.time = time || movie.time;
+        movie.language = language || movie.language;
+        movie.year = year || movie.year;
+        movie.video = video || movie.video;
+        movie.casts = casts || movie.casts;
+        const updatedMovie = await movie.save();
+  
+        res.status(201).json(updatedMovie);
+      } else {
+        res.status(404);
+        throw new Error("Movie not found");
+      }
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  };
+  export {createMovie, getMovieById, updateMovie}
--- server/src/Routes/movie.router.js
+++ server/src/Routes/movie.router.js
@@ -1,8 +1,11 @@
 import express from "express";
-import {createMovie, getMovieById} from "../Controller/movie.controller.js";
+import {createMovie, getMovieById,updateMovie} from "../Controller/movie.controller.js";
 const router = express.Router();
 
 
 router.post("/", createMovie);
 router.get("/:id", getMovieById)
+router.put("/:id", updateMovie)
+
+
 export default router;
