f60d404b48a3e47bed7bde5b54bd6e2143fcbfb9
iamhaao <haolee2002tst@gmail.com>
2024-03-17 12:56:59 +0700

    Fix flash in Hover

--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -2,8 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FaHeart } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
+import { IfMovieLiked, LikeMovie } from "../context/Functionaltes";
 function Movie({ movie }) {
   const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
+  const isLiked = (movie) => {
+    return IfMovieLiked(movie);
+  };
+  console.log(isLiked(movie));
 
   return (
     <>
@@ -18,8 +24,10 @@ function Movie({ movie }) {
         <div className="absolute flex-btn gap-2 bottom-0 right-0 left-0 bg-main bg-opacity-60 text-white px-4 py-2 ">
           <h3 className="font-semibold  truncate ">{movie.name}</h3>
           <button
+            onClick={() => LikeMovie(movie, dispatch, currentUser)}
             className={`h-9 w-9 text-sm flex-colo transitions
-bg-transparent hover:bg-subMain border-2 border-subMain rounded-md bg-subMain text-white`}
+        ${isLiked(movie) ? "bg-subMain" : "bg-transparent"}
+        hover:bg-subMain border-2 border-subMain rounded-md  text-white`}
           >
             <FaHeart />
           </button>
